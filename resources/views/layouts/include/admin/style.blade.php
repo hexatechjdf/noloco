b6fa2a00c9c53956970ea7678e729e60cfb54b9a
zainhts <zain4bee@gmail.com>
2025-04-10 15:15:26 +0500

    changes

--- resources/views/layouts/include/admin/style.blade.php
+++ resources/views/layouts/include/admin/style.blade.php
@@ -16,112 +16,127 @@
 <link href="{{ asset('admin/assets/dashboard/css/style.css') }}" rel="stylesheet">
 
 <style>
-     .show_values {
-            cursor: pointer;
-        }
+    .show_values {
+        cursor: pointer;
+    }
 
-        .mappingPicker :is(input#searchBar, input#searchBar:focus) {
-            border: none;
-            box-shadow: unset;
-        }
+    .mappingPicker :is(input#searchBar, input#searchBar:focus) {
+        border: none;
+        box-shadow: unset;
+    }
 
-        .mappingPicker li.list-group-item {
-            display: flex;
-            cursor: pointer;
-            justify-content: space-between;
-        }
+    .mappingPicker li.list-group-item {
+        display: flex;
+        cursor: pointer;
+        justify-content: space-between;
+    }
 
-        .mappingPicker .allList {
-            margin: unset;
-            padding: unset;
-        }
+    .mappingPicker .allList {
+        margin: unset;
+        padding: unset;
+    }
 
-        .mappingPicker .jsonList {
-            width: 100%;
-            border-radius: unset;
-        }
+    .mappingPicker .jsonList {
+        width: 100%;
+        border-radius: unset;
+    }
 
-        .mappingPicker.hidden>ul {
-            display: none;
-        }
+    .mappingPicker.hidden>ul {
+        display: none;
+    }
 
-        .mappingPicker {
-            position: relative;
-        }
+    .mappingPicker {
+        position: relative;
+    }
 
-        .mappingPicker ul.list-group.listPicker.absolute {
-            position: absolute;
-            z-index: 99999999;
-        }
+    .mappingPicker ul.list-group.listPicker.absolute {
+        position: absolute;
+        z-index: 99999999;
+    }
 
-        .mappingPicker li.list-group-item .close {
-            text-align: center;
-            width: 100%;
-        }
-        #loader-overlay {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: rgba(0, 0, 0, 0.7); /* Dark transparent background */
-    z-index: 999999;
-    display: none; /* Hidden by default */
-    align-items: center;
-    justify-content: center;
-}
+    .mappingPicker li.list-group-item .close {
+        text-align: center;
+        width: 100%;
+    }
 
-.loader-container {
-    text-align: center;
-    background: rgba(255, 255, 255, 0.1); /* Slightly transparent background */
-    padding: 20px;
-    border-radius: 10px;
-}
+    #loader-overlay {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(0, 0, 0, 0.7);
+        /* Dark transparent background */
+        z-index: 999999;
+        display: none;
+        /* Hidden by default */
+        align-items: center;
+        justify-content: center;
+    }
 
-.form-control{
-    display: block;
-  width: 100%;
-  padding: .375rem .75rem;
-  font-size: 1rem;
-  font-weight: 400;
-  line-height: 1.5;
-  color: #212529;
-  background-color: #fff !important;
-  background-clip: padding-box;
-  border: 1px solid #ced4da !important;
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  appearance: none;
-  border-radius: .375rem !important;
-  transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
-}
-label {
-    color: #6c757d !important; /* Light gray color */
-    margin-bottom:1px;
-}
-.card-title h5{
-    font-size: 1.5rem; /* Adjust size as needed */
-    font-weight: bold; /* Make it bold */
-    color: #343a40;
-}
-.form-head{
-    font-size: 1rem; /* Adjust size as needed */
-    font-weight: bold; /* Make it bold */
-    color: #343a40;
-    margin-bottom:5px;
-}
-.select2-selection.select2-selection--single {
-  height: 37px !important;
-}
-.select2-container--default .select2-selection--single .select2-selection__rendered {
-  line-height: 34px !important;
-}
-.select2-container--default .select2-selection--single .select2-selection__arrow {
-  top: 6px !important;
-}
-.select2-container--default .select2-selection--single {
-  border: 1px solid #ced4da !important;
-  border-radius: .375rem !important;
-}
+    .loader-container {
+        text-align: center;
+        background: rgba(255, 255, 255, 0.1);
+        /* Slightly transparent background */
+        padding: 20px;
+        border-radius: 10px;
+    }
 
+    .form-control {
+        display: block;
+        width: 100%;
+        padding: .375rem .75rem;
+        font-size: 1rem;
+        font-weight: 400;
+        line-height: 1.5;
+        color: #212529;
+        background-color: #fff !important;
+        background-clip: padding-box;
+        border: 1px solid #ced4da !important;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        appearance: none;
+        border-radius: .375rem !important;
+        transition: border-color .15s ease-in-out, box-shadow .15s ease-in-out;
+    }
+
+    label {
+        color: #6c757d !important;
+        /* Light gray color */
+        margin-bottom: 1px;
+    }
+
+    .card-title h5 {
+        font-size: 1.5rem;
+        /* Adjust size as needed */
+        font-weight: bold;
+        /* Make it bold */
+        color: #343a40;
+    }
+
+    .form-head {
+        font-size: 1rem;
+        /* Adjust size as needed */
+        font-weight: bold;
+        /* Make it bold */
+        color: #343a40;
+        margin-bottom: 5px;
+    }
+
+    .select2-selection.select2-selection--single {
+        height: 37px !important;
+    }
+
+    .select2-container--default .select2-selection--single .select2-selection__rendered {
+        line-height: 34px !important;
+    }
+
+    .select2-container--default .select2-selection--single .select2-selection__arrow {
+        top: 6px !important;
+    }
+
+    .select2-container--default .select2-selection--single {
+        border: 1px solid #ced4da !important;
+        border-radius: .375rem !important;
+    }
 </style>
